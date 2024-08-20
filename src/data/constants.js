727496e2b561fbf6d5a25e66e2bc60dd53418f1d
Afsal  Suban <afsal.s@noblq.com>
2024-08-20 12:05:39 +0530

    created a portfolio web app

---
+++ src/data/constants.js
@@ -0,0 +1,228 @@
+export const Bio = {
+    name: "Afsal S",
+    roles: [
+      "AI Developer",
+    
+    ],
+    description:
+      "As an AI developer proficient in building Chatbots and with a solid background in ML for forecast prediction, I bring a unique blend of skills to the table. My proven track record in both Java and Python development, coupled with my expertise in creating seamless web applications, positions me as a valuable asset in the dynamic world of development. Passionate about innovation, I am eager to collaborate on challenging projects and deliver high-quality solutions that push the boundaries of technology. Let's connect and explore opportunities to drive innovation together.",
+    github: "https://github.com/afsals2001",
+    resume:
+      "https://drive.google.com/file/d/1Ciqq47Qlvf4Y7OMdGi4wCCXfop6pGOLY/view?usp=drivesdk",
+    linkedin: "https://www.linkedin.com/in/afsal2001/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+            name: "Spring Boot",
+            image:
+              "https://img.icons8.com/color/140/spring-logo.png",
+          },
+          {
+            name: "Machine Learning",
+            image:
+              "https://www.pinclipart.com/picdir/middle/520-5208843_brain-learning-png-clipart-artificial-intelligence-logo-png.png",
+          },
+          
+          
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        
+        {
+          name: "XML",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+        },
+        {
+          name: "Android Studio",
+          image:
+            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://brandlogos.net/wp-content/uploads/2023/09/postman-logo_brandlogos.net_dsr1l-512x510.png",
+        },
+        {
+            name: "SOAPUI",
+            image:
+              "https://dl.flathub.org/repo/appstream/x86_64/icons/128x128/org.soapui.SoapUI.png",
+          },
+          {
+            name: "Jupyter Notebook",
+            image:
+              "https://addo.ai/wp-content/uploads/2023/06/Jupyter.png",
+          },
+       
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://noblq.com/wp-content/uploads/2024/01/logo-white.svg",
+      role: "AI Developer",
+      company: "Noblq Pvt Ltd",
+      date: "Aug 2023 - Present",
+      desc: "Working on predicting future data. My role includes automating the overall flow of the process",
+      skills: [
+        "AI",
+        "Chatbot Development",
+        "Machine Learning",
+        "Prediction",
+        "Prefect Dataflow Automation",
+        "DBT"
+       
+      ],
+   
+    },
+    {
+      id: 0,
+      img: "https://noblq.com/wp-content/uploads/2024/01/logo-white.svg",
+      role: "Java Developer",
+      company: "Noblq Pvt Ltd",
+      date: "Jan 2023 - July 2023",
+      desc: "I am working as an Android and Java developer. My role involves the SAP Byd adapter part.",
+      skills: [
+        "Java",
+        "JDBC",
+        "Frontend Development",
+        "Spring boot"
+      
+      ],
+    
+    },
+    
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://avsenggcollege.ac.in/img/logoonly.png",
+      school: "AVS Engineering College",
+      date: "Aug 2019 - Aug 2023",
+      grade: "8.66 CGPA",
+      degree: "Bachelor of Engineering - BE, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://th.bing.com/th/id/OIP.6AahXGQCyxVCzDoUaRduKwHaHa?rs=1&pid=ImgDetMain",
+      school: "Pachamuthu Matric Hr Sec School",
+      date: "Mar 2018 - Mar 2019",
+      grade: "79%",
+      degree: "HSC(XII), Biology with Mathematics",
+    },
+    {
+      id: 2,
+      img: "https://th.bing.com/th/id/OIP.6AahXGQCyxVCzDoUaRduKwHaHa?rs=1&pid=ImgDetMain",
+      school: "Pachamuthu Matric Hr Sec School",
+      date: "Mar 2016 - Mar 2017",
+      grade: "98%",
+      degree: "SSLC(X), Biology with Mathematics",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Online Examination Andriod APP",
+      description:
+        "Through this application , users can take exams in an online ,secure format and receive immediate results.",
+      
+      tags: [
+      "Java",
+      "Andriod Studio",
+      ],
+      category: "Andriod APP's",
+   
+    },
+    {
+      id: 0,
+      title: "Machine Learning Based Vehicle Performance Analyser",
+      description:
+        "Developed with IBM’s assistance ,utilizes advanced algorithms to analyze vehicle metrices for insights into fuel efficiency and operational health.",
+     
+      tags: ["Machine Learning","Python","Github","Pandas","Jupyter Notebook"],
+      category: "web app",
+    
+      
+    },
+    {
+      id: 1,
+      title: "Real Time Facial Emotion Recognition System",
+      description:
+        "Implemented a cutting-edge web application utilizing python machine learning and webcam technology to capture and analyze human facial reactions, providing real-time insights into user emotions and reactions through facial expression recognition.",
+      
+      tags: [
+        "React Js",
+        "MongoDb",
+        "Python",
+        "OpenCV",
+        "Computer Vision",
+      ],
+      category: "web app",
+      
+    
+    },
+    
+  ];
